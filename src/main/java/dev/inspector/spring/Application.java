bc693210a160a99028c7e9a4b230325401c9e9f6
simone.spinelli <simone.spinelli@parsec326.it>
2024-03-19 17:23:18 +0100

    Aggiunta la logica per recuperare i path dallo yaml del progetto che implementa la libreria

--- src/main/java/dev/inspector/spring/Application.java
+++ src/main/java/dev/inspector/spring/Application.java
@@ -1,12 +1,15 @@
 package dev.inspector.spring;
 
+import dev.inspector.spring.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(ConfigProperties.class)
 //@EnableJms
 public class Application {
 
--- src/main/java/dev/inspector/spring/interceptors/rest/RestInterceptor.java
+++ src/main/java/dev/inspector/spring/interceptors/rest/RestInterceptor.java
@@ -1,7 +1,9 @@
 package dev.inspector.spring.interceptors.rest;
 
+import dev.inspector.spring.config.ConfigProperties;
 import dev.inspector.spring.inspectors.RestInspector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
@@ -15,16 +17,24 @@ public class RestInterceptor implements HandlerInterceptor {
     @Autowired
     private RestInspector restInspector;
 
+    @Autowired
+    private ConfigProperties configProperties;
+
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // Extract the original URI template
         String pattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
 
-        if (pattern != null) {
+        // VERIFICO SE IL PATH E' PRESENTE TRA QUELLI PASSATI DALLO YAML DEL PROGETTO CHE IMPLEMENTA LA LIBRERIA
+        if (pattern != null & configProperties.getPathList().contains(pattern)) {
             // Use the pattern as needed, for example, logging or transaction naming
-            restInspector.createTransaction("REST Transaction: " + pattern);
+
+            // STAMPO TUTTI I PATH CONFIGURATI NELLO YAML DEL PROGETTO CHE IMPLEMENTA LA LIBRERIA
+            configProperties.getPathList().forEach(x -> System.out.println(x));
+
+            restInspector.createTransaction(String.format("%s - %s", request.getMethod(), pattern));
         } else {
             System.out.println("Incoming REST request intercepted.");
-            restInspector.createTransaction("REST Transaction");
+            restInspector.createTransaction(String.format("%s - %s", request.getMethod(), pattern));
         }
 
         return true;
