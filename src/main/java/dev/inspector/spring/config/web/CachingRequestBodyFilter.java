d5ab6eca086891b36dc6726835740a96d730e8ff
grove693 <anghel.aranyos@yahoo.com>
2025-02-04 21:02:51 +0200

    Add support for multipart requests on http interceptor

--- src/main/java/dev/inspector/spring/config/web/CachingRequestBodyFilter.java
+++ src/main/java/dev/inspector/spring/config/web/CachingRequestBodyFilter.java
@@ -1,9 +1,9 @@
 package dev.inspector.spring.config.web;
 
-import dev.inspector.spring.utils.http.request.CachedBodyHttpServletRequest;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.io.IOException;
 
@@ -13,7 +13,7 @@ public class CachingRequestBodyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
-            HttpServletRequest requestWithCachedBody = new CachedBodyHttpServletRequest((HttpServletRequest) request);
+            HttpServletRequest requestWithCachedBody = new ContentCachingRequestWrapper((HttpServletRequest) request);
 
             chain.doFilter(requestWithCachedBody, response);
         } else {
